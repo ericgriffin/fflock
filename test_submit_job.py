eb3bbc3e71eb160ff41c9b46b97ffd7c2ac272b9
eric <ericgriffin@gmail.com>
2013-10-11 19:38:50 -0400

    added test_submit_job.py and job_example.job

---
+++ test_submit_job.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+
+import globals
+from datetime import datetime
+import utility
+from subprocess import PIPE, Popen
+import signal
+import sys
+import getopt
+
+
+def find_server_for_storage_job():
+    """
+
+
+
+    @rtype : storage server uuid
+    @return:
+    """
+    return ""
+
+
+def find_server_for_slave_job():
+    """
+
+
+
+    @rtype : slave server uuid
+    @return:
+    """
+    slaveserveruuid = "NA"
+    db = utility.dbconnect()
+    slavecursor = db.cursor()
+    slavecursor.execute("SELECT UUID, ServerType, State FROM Servers WHERE ServerType = %s", "Slave")
+    slaveresults = slavecursor.fetchall()
+
+    #find best slave server to assign the job
+    shortest_queue = 1000000
+    server_with_shortest_queue = ""
+    for slaverow in slaveresults:
+        current_queue = 0
+        slaveserveruuid = slaverow[0]
+        jobcursor = db.cursor()
+        jobcursor.execute("SELECT JobType, Assigned, Active, AssignedServerUUID, Priority, Dependencies, Progress FROM Jobs WHERE AssignedServerUUID = %s", str(slaveserveruuid))
+        jobresults = jobcursor.fetchall()
+        for jobrow in jobresults:
+            current_queue += 1
+        if current_queue < shortest_queue:
+            server_with_shortest_queue = slaveserveruuid
+            shortest_queue = current_queue
+    return server_with_shortest_queue
+
+
+def find_storage_UUID_for_job():
+    """
+
+
+
+    @rtype : storage uuid
+    @return:
+    """
+    storageuuid = ""
+    db = utility.dbconnect()
+    storagecursor = db.cursor()
+    storagecursor.execute("SELECT UUID FROM Storage WHERE StorageType = %s", ("NFS"))
+    storageresults = storagecursor.fetchall()
+    for storagerow in storageresults:
+        storageuuid = storagerow[0]
+    return storageuuid
+
+
+def submit_job(jobtype, command, commandoptions, input, output):
+    """
+
+
+    @rtype : boolean
+    @param jobtype:
+    @param command:
+    @param commandoptions:
+    @param input:
+    @param output:
+    """
+    dependencies = ""
+    db = utility.dbconnect()
+
+    storageuuid = find_storage_UUID_for_job()
+
+    assignedserveruuid = "NA"
+    if jobtype == "Slave":
+        assignedserveruuid = find_server_for_slave_job()
+    elif jobtype == "Storage":
+        assignedserveruuid = find_server_for_storage_job()
+
+    jobuuid = utility.get_uuid()
+    jobinputcursor = db.cursor()
+    jobinputcursor.execute(
+        "INSERT INTO Jobs(UUID, JobType, Command, CommandOptions, JobInput, JobOutput, Assigned, Active, AssignedServerUUID, StorageUUID, MasterUUID, Priority, Dependencies, Progress, AssignedTime, CreatedTime) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+        (jobuuid, jobtype, command, commandoptions, input, output, 1, 0, assignedserveruuid, storageuuid, "0", 1, dependencies, 0, _timestamp, _timestamp))
+    db.close()
+    return True
+
+
+def main(argv):
+    """
+
+
+    @rtype : none
+    @param argv:
+    """
+
+    try:
+        opts, args = getopt.getopt(argv, "hd:", ["help", "database="])
+    except getopt.GetoptError:
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            sys.exit()
+        if opt in ("-d", "--database"):
+            globals.DATABASE_HOST = arg.split(':', 1)[0]
+            globals.DATABASE_PORT = arg.split(':', 1)[-1]
+            if globals.DATABASE_PORT == globals.DATABASE_HOST:
+                globals.DATABASE_PORT = 3306
+
+    jobtype = "Slave"
+    command = "yes | ffmpeg %s -i %s %s"
+    commandoptions = ""
+    input = "test.mov"
+    output = "Final.mp4"
+
+    #submit_job(jobtype, command, commandoptions, input, output)
+    #submit_job("Slave", "yes | ffmpeg %s -i %s %s", "", "1.mp4", "Final1.avi")
+    submit_job("Slave", "yes | ffmpeg %s -i %s %s", "", "test.mov", "Final.mp4")
+
+
+if __name__ == "__main__":
+    _uuid = utility.get_uuid()
+    _timestamp = datetime.now()
+    main(sys.argv[1:])
